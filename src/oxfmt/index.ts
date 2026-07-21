aaa41cf242f692126ca741cd0f5a9b35f48c3316
Sam Chung <samc@seek.com.au>
2026-07-21 12:29:28 +1000

    Ryan must have order (#8)

--- src/oxfmt/index.ts
+++ src/oxfmt/index.ts
@@ -5,7 +5,9 @@ export const defaults = {
   tabWidth: 2,
   trailingComma: 'all',
   printWidth: 80,
-  sortPackageJson: true,
+  sortPackageJson: {
+    sortScripts: true,
+  },
   ignorePatterns: [
     '/.gantry/**/*.yaml',
     '/.gantry/**/*.yml',
